d50483b0e56a9fef65540b1127b34a38f0fd5e95
Dimasblekk <137034636+Dimasblekk@users.noreply.github.com>
2023-06-19 13:57:31 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title= />
+        <Header title= Berita />
         <p className="description">
          “Dengan mengikuti The 13th IHRS 2023, para peserta mempunyai kesempatan untuk menggali tren, teknologi terkini, serta strategi utama di berbagai ruang sumber daya manusia yang berubah dengan cepat.
         </p>
